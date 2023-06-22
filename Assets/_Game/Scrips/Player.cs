580dcd9fa2e6b1eb1ca985ca931b747a59930c55
taichuan2002 <134790352+taichuan2002@users.noreply.github.com>
2023-06-22 20:16:06 +0700

    sử lý di chuyển + va chạm

--- Assets/_Game/Scrips/Player.cs
+++ Assets/_Game/Scrips/Player.cs
@@ -1,3 +1,4 @@
+using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.Pipes;
@@ -5,12 +6,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Vector3 Startpoint;
     [SerializeField] private float speed;
-    public enum Direct { None, Left, Right, Up, Down};
-    public Direct direct = Direct.None;
-    public float minSwipeDistance = 30f;
-    private Vector2 startPos;
+    [SerializeField] LayerMask wallLayer;
+
+    private bool isMoving = false;
+    private Vector3 currentPos;
+    private Vector3 targetPos;
+
     void Start()
     {
         
@@ -18,56 +20,85 @@ public class Player : MonoBehaviour
     void Update()
     {
         Control();
-        //transform.Translate(speed * - Time.deltaTime, 0f, 0f);
-
     }
 
 
     public void Control()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (!isMoving)
         {
-            Vector2 endPos = Input.mousePosition;
-            Vector2 swipeDirection = endPos - startPos;
-            /*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, 5f, brickLayer))
-            {*/
-                if (swipeDirection.magnitude >= minSwipeDistance)
+            if (Input.GetMouseButtonDown(0))
             {
-                swipeDirection.Normalize();
+                currentPos = Input.mousePosition;
+                //Debug.Log("Current: " + currentPos.x + ":" + currentPos.y + ":" + currentPos.z);
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                targetPos = Input.mousePosition;
+                //Debug.Log("Target: " + targetPos.x + ":" + targetPos.y + ":" + targetPos.z);
+                Vector3 moveDir = targetPos - currentPos;
+                moveDir = GetDirection(moveDir);
+                //Debug.Log("Direction: " + moveDir.x + ":" + moveDir.y + ":" + moveDir.z);
+                if (moveDir != Vector3.zero)
+                {
+                    StopAllCoroutines();
+                    StartCoroutine(Move(moveDir));
+                    StartCoroutine(CheckWall(moveDir));
+                }
+            }
+        }
+    }
+    private Vector3 GetDirection(Vector3 direction)
+    {
+        float horizontal = Mathf.RoundToInt(direction.x);
+        float vertical = Mathf.RoundToInt(direction.y);
 
-                if (Mathf.Abs(swipeDirection.x) > Mathf.Abs(swipeDirection.y))
-                {
-                    if (swipeDirection.x > 0)
-                    {
-                        direct = Direct.Right;
-                        Debug.Log("R");
-                        transform.Translate(speed * Time.deltaTime, 0f, 0f);
-                    }
-                    else
-                    {
-                        Debug.Log("L");
-                        direct = Direct.Left;
-                        transform.Translate(speed * -Time.deltaTime, 0f, 0f);
-                    }
-                }
-                else
-                {
-                    if (swipeDirection.y > 0)
-                    {
-                        direct = Direct.Up;
-                    }
-                    else
-                    {
-                        direct = Direct.Down;
-                    }
-                }
-            }
-            else
+        if (Mathf.Abs(horizontal) > Mathf.Abs(vertical))
+        {
+            return new Vector3(horizontal * 3f, 0f, 0f).normalized;
+        }
+        else if (Mathf.Abs(vertical) > Mathf.Abs(horizontal))
+        {
+            return new Vector3(0f, 0f, vertical * 3f).normalized;
+        }
+        else
+        {
+            return new Vector3(0f, 0f, 0f);
+        }
+    }
+
+    private IEnumerator Move(Vector3 direction)
+    {
+        while (true)
+        {
+            isMoving = true;
+            Vector3 movement = new Vector3(direction.x, 0f, direction.z);
+            transform.position += movement * speed * Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator CheckWall(Vector3 direction)
+    {
+        while (true)
+        {
+            RaycastHit hit;
+            Vector3 raycastPos = transform.position;
+            if (Physics.Raycast(raycastPos, direction, out hit, 0.5f, wallLayer))
             {
-                direct = Direct.None;
+                StopAllCoroutines();
+                isMoving = false;
             }
+            yield return null;
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "BrickBlock")
+        {
+            Debug.Log("a");
+            Destroy(collision.gameObject);
         }
     }
 }
